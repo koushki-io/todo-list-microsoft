edbca5971c368b39de69595652926d455135d87f
mehdi koushki <mehdi@pro.ai>
2023-11-05 15:36:09 +0330

    navbar

---
+++ src/components/navbar/Navabr.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import styles from './navbar.module.css'
+import WidgetsIcon from '@mui/icons-material/Widgets';
+import SearchIcon from '@mui/icons-material/Search';
+import SettingsSharpIcon from '@mui/icons-material/SettingsSharp';
+import Person4SharpIcon from '@mui/icons-material/Person4Sharp';
+const Navabr = () => {
+    return (
+        <div className={styles.parent}>
+            
+       <div className={styles.item}>
+       <div ><WidgetsIcon style={{color:"#fff"}}/></div>
+        <div>
+            <h3 style={{color:"#fff"}}>To DO</h3>
+        </div>
+       </div>
+
+        <div className={styles.inputBox}>
+        <SearchIcon className={styles.iconSerche} />
+            <input type="search" className={styles.inputSerche}></input>
+        </div>
+
+        <div className={styles.item}>
+        <div> <SettingsSharpIcon style={{color:"#fff"}} /> </div>
+        <div> <Person4SharpIcon style={{color:"#fff"}}/> </div>
+         </div>
+        
+
+        </div>
+    );
+}
+
+export default Navabr;
